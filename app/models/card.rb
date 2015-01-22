3295a1a63f878b7d6f49f5bd5cc42d1e60e27c70
DmitriyManaev <manaev@red-tie.ru>
2015-01-22 14:52:54 +0500

    Delete unnecesary files and update card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
-  before_save :get_review_date
+  before_create :get_review_date
 
   private
     def get_review_date
