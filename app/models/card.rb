13cc56d497005802f77ca1caf72b950a051e2be7
DmitriyManaev <manaev@red-tie.ru>
2015-01-22 15:07:11 +0500

    Set review date method in card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,9 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
-  before_create :get_review_date
+  before_create :set_review_date
 
   private
-    def get_review_date
+    def set_review_date
       self.review_date = Time.now
     end
 end
