9462d545a2b9c1c0b97be7f724abb2e583942c15
DmitriyManaev <manaev@red-tie.ru>
2015-01-28 22:29:22 +0500

    Hound corrects

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,20 +1,21 @@
 require "rails_helper"
-
-describe "Home", :type => :feature  do
-  let!(:card) { Card.create(original_text: "example", translated_text: "пример") }
+describe "Home", type: :feature  do
+  let!(:card) { Card.create(original_text: "example",
+              translated_text: "пример")
+  }
 
   before :each do
     visit root_path
   end
 
   it "check card with right answer" do
-    fill_in "translated_text", :with => "пример"
+    fill_in "translated_text", with: "пример"
     click_button "Проверить"
     expect(page.find("#alert-success")).to have_content "Правильно"
   end
 
   it "check card with wrong answer" do
-    fill_in "translated_text", :with => "не правильный ответ"
+    fill_in "translated_text", with: "не правильный ответ"
     click_button "Проверить"
     expect(page.find("#alert-fail")).to have_content "Не правильно"
   end
