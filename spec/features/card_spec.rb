ba6f1308146da8b63557c1f7dd7671ebf95e8b68
DmitriyManaev <manaev@red-tie.ru>
2015-01-29 09:35:52 +0500

    Rename feature spec for cards

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,19 +1,19 @@
 require "rails_helper"
-describe "Home", type: :feature  do
+describe "Check card on home page" do
   before :each do
     FactoryGirl.create(:card)
     visit root_path
   end
 
-  it "check card with right answer" do
+  it "with right answer" do
     fill_in "translated_text", with: "пример"
     click_button "Проверить"
-    expect(page.find("#alert-success")).to have_content "Правильно"
+    expect(page).to have_content "Правильно"
   end
 
-  it "check card with wrong answer" do
+  it "with wrong answer" do
     fill_in "translated_text", with: "не правильный ответ"
     click_button "Проверить"
-    expect(page.find("#alert-fail")).to have_content "Не правильно"
+    expect(page).to have_content "Не правильно"
   end
 end
