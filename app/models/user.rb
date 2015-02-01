90098008107ebafe2a35323e73abb2330b2f88c5
DmitriyManaev <manaev@red-tie.ru>
2015-02-01 23:48:23 +0500

    Corrects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-  authenticates_with_sorcery!
-  has_many :authentications, dependent: :destroy
-  accepts_nested_attributes_for :authentications
   validates :password, length: { minimum: 6 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true
+  has_many :authentications, dependent: :destroy
   has_many :cards, dependent: :destroy
+  authenticates_with_sorcery!
+  accepts_nested_attributes_for :authentications
 end
