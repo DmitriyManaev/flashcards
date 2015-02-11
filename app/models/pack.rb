91fead423bd81d49907d8d872753f7b0547f5c62
DmitriyManaev <manaev@red-tie.ru>
2015-02-12 00:42:37 +0500

    Sexy validates

--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,18 +1,18 @@
 class Pack < ActiveRecord::Base
   validates :title, :user_id, presence: true
-  validates_uniqueness_of :title, scope: :user_id, on: :create
+  validates :title, uniqueness: { scope: :user_id }
   has_many :cards, dependent: :destroy
   belongs_to :user, required: true
   mount_uploader :image, ImageUploader
   after_destroy :reset_current_pack
 
   def current?
-    self.user.current_pack == self
+    user.current_pack == self
   end
 
   def reset_current_pack
-    if self.id == self.user.current_pack_id
-      self.user.update_attribute(:current_pack_id, nil)
+    if id == user.current_pack_id
+      user.update_attribute(:current_pack_id, nil)
     end
   end
 end
