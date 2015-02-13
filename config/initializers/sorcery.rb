8d6511ab14c20b63546fb42bc43c46b9d2e18081
DmitriyManaev <manaev@red-tie.ru>
2015-02-14 00:52:14 +0500

    Change Sorcery

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -112,7 +112,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.twitter.key = ENV["sorcery_twitter_key"]
   config.twitter.secret = ENV["sorcery_twitter_secret"]
-  config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
+  config.twitter.callback_url = ENV["twitter_callback_url"]
   config.twitter.user_info_mapping = {:email => "screen_name"}
   #
   # config.facebook.key = ""
