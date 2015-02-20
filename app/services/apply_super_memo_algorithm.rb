f90e15283afd56645798b65722cd5bae87f86415
DmitriyManaev <manaev@red-tie.ru>
2015-02-20 15:35:29 +0500

    Hound corrects3

--- app/services/apply_super_memo_algorithm.rb
+++ app/services/apply_super_memo_algorithm.rb
@@ -3,20 +3,15 @@ class ApplySuperMemoAlgorithm
     quality_answer = get_quality_answer(time_answer.to_i)
     quality_answer -= 3 if !answer
     e_factor = get_e_factor(card.e_factor, quality_answer)
+    interval = number_of_review = 0
     if answer
-      interval = 0
       if quality_answer >= 4
         interval = get_interval(card.interval_to_review,
                                 card.number_of_review + 1,
                                 e_factor)
       end
-      update_card(card, interval, card.number_of_review + 1, e_factor)
-    else
-      update_card(card, 0, 0, e_factor)
+      number_of_review = card.number_of_review + 1
     end
-  end
-
-  def self.update_card(card, interval, number_of_review, e_factor)
     card.update_attributes(review_date: Time.now + interval.days,
                            interval_to_review: interval,
                            number_of_review: number_of_review,
@@ -37,7 +32,7 @@ class ApplySuperMemoAlgorithm
     e_factor += (0.1 - (5 - q) * (0.08 + (5 - q) * 0.02))
     e_factor = 1.3 if e_factor < 1.3
     e_factor = 2.5 if e_factor > 2.5
-    return e_factor
+    e_factor
   end
 
   def self.get_interval(interval, number_of_review, e_factor)
