0dcf43cb107749e02660f8b23901289ce6d3c730
DmitriyManaev <manaev@red-tie.ru>
2015-02-27 22:56:08 +0500

    double ajax success

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,14 @@
 setInterval(function(){
   $("#answer_time").val(parseInt($("#answer_time").val(), 10) + 1);
 }, 1000);
+
+$(document).ready(function(){
+  $(".review-form").bind("ajax:success",
+    function(){
+      $.ajax({
+        url: "/",
+        dataType: "script"
+      });
+    }
+  );
+});
