bda5737024654cb2f334b373b0a1c97b6405dd7c
DmitriyManaev <manaev@red-tie.ru>
2015-02-27 11:32:28 +0500

    singular resources

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
-class ReviewController < ApplicationController
-  def review_card
+class ReviewsController < ApplicationController
+  def create
     @card = current_user.cards.find(params[:card_id])
     if @card.correct_answer(params[:translated_text], params[:answer_time])
       flash.now[:success] = "Правильно"
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   resources :packs do
     resources :cards
   end
+  resource :review
   get "login", to: "user_sessions#new"
   get "logout", to: "user_sessions#destroy"
   get "signup", to: "users#new"
-  post "review", to: "review#review_card"
   get "get_card_for_review", to: "cards#get_card_for_review"
   match "oauth/callback", to: "oauths#callback", via: [:get, :post]
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
